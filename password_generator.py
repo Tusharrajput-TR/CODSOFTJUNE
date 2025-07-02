8f8651baee3693880d4ee2e8b3c8ab3a3fae16a2
Tushar Rajput <tusharrajput999666@gmail.com>
2025-07-02 16:13:30 +0530

    Delete password_generator.py

--- password_generator.py
+++
@@ -1,9 +0,0 @@
-import random
-chars="abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()"
-length=int(input("Enter Length: "))
-password=""
-for i in range(length):
-    password+=random.choice(chars)
-print(password)
-
-
