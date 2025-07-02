d2d0c3f6f2cf84ce80a0f46e86c195c30eac6eda
Tushar Rajput <tusharrajput999666@gmail.com>
2025-07-02 16:05:47 +0530

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -1,6 +1,6 @@
 import random
 chars="abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()"
-length-int(input("Enter Length: "))
+length=int(input("Enter Length: "))
 password=""
 for i in range(length):
     password+=random.choice(chars)
