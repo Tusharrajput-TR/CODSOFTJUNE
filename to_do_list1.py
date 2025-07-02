9f9c3da2b31d6bee93c7721bf4054725d235ea33
Tushar Rajput <tusharrajput999666@gmail.com>
2025-07-02 16:31:51 +0530

    Add files via upload

---
+++ to_do_list1.py
@@ -0,0 +1,30 @@
+task_lst=[]
+def add_task():
+    enter_task=input("Enter the task:")
+    task_lst.append(enter_task)
+def remove_task():
+    remove_task=input("Enter the task:")
+    if remove_task in task_lst:
+        task_lst.remove(remove_task)
+    else:
+        print("The task doesn't exist")
+def view_task():
+    print(task_lst)
+
+while True :
+    print("Main Menu")
+    print("1. Add task")
+    print("2. Remove task")
+    print("3. View task")
+    print("4. Exit")
+    ch=int(input("Enter your choice:"))
+    if ch==1:
+        add_task()
+    elif ch==2:
+        remove_task()
+    elif ch==3:
+        view_task()
+    elif ch==4:
+        break
+    else:
+        print("Invalid choice,Try again!")
